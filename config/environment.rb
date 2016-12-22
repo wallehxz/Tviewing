8df24ffb22095d3007c571d28d87c575b32234ae
Koogle.Zhang <koogle.zhang@gmail.com>
2016-12-22 10:28:53 +0800

    去掉中间件

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-require 'rack/cache'
-Rails.application.middleware.use Rack::Cache,
-   verbose: true,
-   metastore: Settings.rake_cache_meta,
-   entitystore: Settings.rake_cache_body
+# require 'rack/cache'
+# Rails.application.middleware.use Rack::Cache,
+#    verbose: true,
+#    metastore: Settings.rake_cache_meta,
+#    entitystore: Settings.rake_cache_body
 
 require File.expand_path('../application', __FILE__)
 
