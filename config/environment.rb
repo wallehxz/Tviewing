cd110511d07bcc7dea0ab0cf8d61b78848bdd36b
Koogle.Zhang <koogle.zhang@gmail.com>
2016-12-22 10:06:23 +0800

    添加 rackcache

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
-config.middleware.use Rack::Cache,
-   verbose:     true,
-   metastore:   Settings.rake_cache_meta,
-   entitystore: Settings.rake_cache_meta
+require 'rack/cache'
+Rails.application.middleware.use Rack::Cache,
+   verbose: true,
+   metastore: Settings.rake_cache_meta,
+   entitystore: Settings.rake_cache_body
 
 require File.expand_path('../application', __FILE__)
 
