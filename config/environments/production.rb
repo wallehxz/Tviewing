c58976ea134a2e6ef7d724d2d7161f09786d6c5d
Koogle.Zhang <koogle.zhang@gmail.com>
2016-12-22 12:07:36 +0800

    配置更名

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,6 @@ Rails.application.configure do
 
   config.static_cache_control = "public, max-age=2592000" # changed
 
-  config.serve_static_assets = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
