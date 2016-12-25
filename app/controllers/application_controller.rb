b91283ab173e7a3b3622a9cc12435170acf8c3a8
KoogleZhang <koogle.zhang@gmail.com>
2016-12-25 19:34:16 +0800

    添加跳转链接

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   #protect_from_forgery with: :exception
-
+  before_filter :redirect_to_https
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
       respond_to do |format|
@@ -10,6 +10,16 @@ class ApplicationController < ActionController::Base
     else
       respond_to do |format|
         format.html { redirect_to sign_in_path }
+      end
+    end
+  end
+
+  def redirect_to_https
+    if Rails.env == 'production'
+      if action_name != 'show'
+        redirect_to :protocol=> 'https://', :host=>'www.koogle.cc'
+      else
+        redirect_to :protocol=> 'http://', :host=>'www.koogle.cc'
       end
     end
   end
