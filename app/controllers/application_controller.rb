82f4e5edabdaf2141728dd8ba225adba78db767c
KoogleZhang <koogle.zhang@gmail.com>
2016-12-25 21:32:11 +0800

    添加强制

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
 
   #protect_from_forgery with: :exception
-  before_filter :redirect_to_https
   rescue_from CanCan::AccessDenied do |exception|
     if current_user
       respond_to do |format|
@@ -10,18 +9,6 @@ class ApplicationController < ActionController::Base
     else
       respond_to do |format|
         format.html { redirect_to sign_in_path }
-      end
-    end
-  end
-
-  def redirect_to_https
-    if Rails.env == 'production'
-      if action_name != 'show' && !request.ssl?
-        puts "#{action_name} #{!request.ssl?}"
-        redirect_to :protocol=>'https://'
-      elsif action_name == 'show' && request.ssl?
-        puts "#{action_name} #{request.ssl?}"
-        redirect_to :protocol=>'http://'
       end
     end
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -51,7 +51,7 @@ Rails.application.configure do
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = action_name !='show'
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
