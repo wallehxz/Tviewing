151117d22ea200bbb6fcf54fe6be1fab55f8fee9
Koogle.Zhang <koogle.zhang@gmail.com>
2017-05-25 11:39:01 +0800

    更换部署服务器

--- config/deploy/aliyun.rb
+++ config/deploy/aliyun.rb
@@ -2,8 +2,8 @@ set :branch, 'master'
 set :rails_env, 'production'
 set :rvm_ruby_version, '2.3.2'
 set :rvm_type, :user
-set :deploy_to, '/var/koogle/video'
+set :deploy_to, '/var/www/koogle'
 set :rvm_custom_path, '/home/deploy/.rvm'
 set :rvm_roles, [:app, :web]
 
-server 'deploy@104.238.140.199', user: 'deploy', port: 22, roles: %w[web app db], primary: true
+server 'deploy@101.201.208.108', user: 'deploy', port: 22, roles: %w[web app db], primary: true
