655187a5df603ea856e0e5672f74fffe388db975
Huaxuan Zhang <koogle.zhang@gmail.com>
2020-03-29 10:56:50 +0800

    调整页面样式

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.4.1'
 
-set :application, 'video'
-set :repo_url, 'git@github.com:kooogle/tv.git'
+set :application, 'tviewing'
+set :repo_url, 'https://github.com/kooogle/Tviewing.git'
 set :ssh_options, { keys: %w{~/.ssh/id_rsa}, forward_agent: true, auth_methods: %w(publickey) }
 set :scm, :git
 set :format, :pretty
